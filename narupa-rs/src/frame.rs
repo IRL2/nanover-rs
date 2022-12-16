f1ec4f9e3de07e0c38bd60ce6ebb318b748f4c84
Jonathan Barnoud <jonathan@barnoud.net>
2022-12-16 16:18:34 +0100

    Make clippy a bit happier

--- narupa-rs/src/frame.rs
+++ narupa-rs/src/frame.rs
@@ -6,6 +6,9 @@ use std::collections::HashMap;
 
 pub use crate::proto::protocol::trajectory::FrameData;
 
+#[derive(Debug)]
+pub struct ExistingDataError {}
+
 impl FrameData {
     pub fn empty() -> FrameData {
         let values: HashMap<::prost::alloc::string::String, Value> = HashMap::new();
@@ -14,46 +17,46 @@ impl FrameData {
         FrameData { values, arrays }
     }
 
-    pub fn insert_number_value(&mut self, key: &str, value: f64) -> Result<(), ()> {
+    pub fn insert_number_value(&mut self, key: &str, value: f64) -> Result<(), ExistingDataError> {
         let serialed_value = Value {
             kind: Some(Kind::NumberValue(value)),
         };
         match self.values.insert(key.to_string(), serialed_value) {
             None => Ok(()),
-            Some(_) => Err(()),
+            Some(_) => Err(ExistingDataError {  }),
         }
     }
 
-    pub fn insert_float_array(&mut self, key: &str, value: Vec<f32>) -> Result<(), ()> {
+    pub fn insert_float_array(&mut self, key: &str, value: Vec<f32>) -> Result<(), ExistingDataError> {
         let float_array = Values::FloatValues(FloatArray { values: value });
         let value_array = ValueArray {
             values: Some(float_array),
         };
         match self.arrays.insert(key.to_string(), value_array) {
             None => Ok(()),
-            Some(_) => Err(()),
+            Some(_) => Err(ExistingDataError {  }),
         }
     }
 
-    pub fn insert_index_array(&mut self, key: &str, value: Vec<u32>) -> Result<(), ()> {
+    pub fn insert_index_array(&mut self, key: &str, value: Vec<u32>) -> Result<(), ExistingDataError> {
         let index_array = Values::IndexValues(IndexArray { values: value });
         let value_array = ValueArray {
             values: Some(index_array),
         };
         match self.arrays.insert(key.to_string(), value_array) {
             None => Ok(()),
-            Some(_) => Err(()),
+            Some(_) => Err(ExistingDataError {  }),
         }
     }
 
-    pub fn insert_string_array(&mut self, key: &str, value: Vec<String>) -> Result<(), ()> {
+    pub fn insert_string_array(&mut self, key: &str, value: Vec<String>) -> Result<(), ExistingDataError> {
         let string_array = Values::StringValues(StringArray { values: value });
         let value_array = ValueArray {
             values: Some(string_array),
         };
         match self.arrays.insert(key.to_string(), value_array) {
             None => Ok(()),
-            Some(_) => Err(()),
+            Some(_) => Err(ExistingDataError {  }),
         }
     }
 }
