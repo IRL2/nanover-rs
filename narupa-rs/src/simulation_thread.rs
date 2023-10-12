3655a2fa4249703bf3e360dd0f00666bf0ef958f
Jonathan Barnoud <jonathan@barnoud.net>
2023-10-12 09:47:21 +0200

    More simulation_thread refactoring
    Move the cached user interactions and the frame count inside the
    TrackedTrajectory. These variables were not reset when a new simulation
    was loaded so they could be at odd with the actual state of the
    simulation.
    
    The commit also normalise some integer types to usize, which reduce the
    need to convert between units in various places and gives a less verbose
    code.

--- narupa-rs/src/simulation_thread.rs
+++ narupa-rs/src/simulation_thread.rs
@@ -2,7 +2,6 @@ use log::{error, info, warn};
 use narupa_proto::trajectory::FrameData;
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
-use std::convert::TryInto;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use std::{thread, time};
@@ -21,20 +20,26 @@ use crate::state_interaction::read_forces;
 /// A simulation with all the book keeping required for the thread.
 struct TrackedSimulation {
     simulation: OpenMMSimulation,
+    simulation_frame: usize,
     reset_counter: usize,
     simulation_counter: usize,
     user_forces: CoordMap,
+    user_energies: Vec<(Option<String>, Option<f64>)>,
 }
 
 impl TrackedSimulation {
     fn new(simulation: OpenMMSimulation, simulation_counter: usize) -> Self {
         let user_forces = BTreeMap::new();
+        let user_energies = Vec::new();
         let reset_counter = 0;
+        let simulation_frame = 0;
         Self {
             simulation,
+            simulation_frame,
             reset_counter,
             simulation_counter,
             user_forces,
+            user_energies,
         }
     }
 
@@ -43,8 +48,9 @@ impl TrackedSimulation {
         self.reset_counter += 1;
     }
 
-    fn step(&mut self, n_frames: i32) {
-        self.simulation.step(n_frames);
+    fn step(&mut self, n_frames: usize) {
+        self.simulation.step(n_frames as i32);
+        self.simulation_frame += n_frames;
     }
 
     fn reset_counter(&self) -> usize {
@@ -70,25 +76,36 @@ impl TrackedSimulation {
     fn update_user_forces(&mut self, force_map: CoordMap) {
         self.user_forces = force_map;
     }
-}
 
-fn next_stop(current_frame: u64, frame_interval: u32, force_interval: u32) -> (i32, bool, bool) {
-    let frame_interval: u64 = frame_interval as u64;
-    let force_interval: u64 = force_interval as u64;
-    let next_frame_stop = frame_interval - current_frame % frame_interval;
-    let next_force_stop = force_interval - current_frame % force_interval;
-    match next_frame_stop.cmp(&next_force_stop) {
-        Ordering::Less => (next_frame_stop.try_into().unwrap(), true, false),
-        Ordering::Greater => (next_force_stop.try_into().unwrap(), false, true),
-        Ordering::Equal => (next_frame_stop.try_into().unwrap(), true, true),
+    fn user_energies(&self) -> &[(Option<String>, Option<f64>)] {
+        &self.user_energies
+    }
+
+    fn update_user_energies(&mut self, user_energies: Vec<(Option<String>, Option<f64>)>) {
+        self.user_energies = user_energies;
+    }
+
+    fn simulation_frame(&self) -> usize {
+        self.simulation_frame
     }
 }
 
-fn next_frame_stop(current_frame: u64, frame_interval: u32) -> i32 {
-    let frame_interval: u64 = frame_interval as u64;
-    (frame_interval - current_frame % frame_interval)
-        .try_into()
-        .unwrap()
+fn next_stop(
+    current_frame: usize,
+    frame_interval: usize,
+    force_interval: usize,
+) -> (usize, bool, bool) {
+    let next_frame_stop = frame_interval - current_frame % frame_interval;
+    let next_force_stop = force_interval - current_frame % force_interval;
+    match next_frame_stop.cmp(&next_force_stop) {
+        Ordering::Less => (next_frame_stop, true, false),
+        Ordering::Greater => (next_force_stop, false, true),
+        Ordering::Equal => (next_frame_stop, true, true),
+    }
+}
+
+fn next_frame_stop(current_frame: usize, frame_interval: usize) -> usize {
+    frame_interval - current_frame % frame_interval
 }
 
 fn starting_frame(simulation: &TrackedSimulation) -> FrameData {
@@ -138,12 +155,11 @@ fn add_force_map_to_frame(force_map: &CoordMap, frame: &mut FrameData) {
 
 fn send_regular_frame(
     simulation: &TrackedSimulation,
-    user_energies: &[(Option<String>, Option<f64>)],
     sim_clone: Arc<Mutex<FrameBroadcaster>>,
 ) -> Result<(), BroadcastSendError> {
     let mut frame = simulation.simulation.to_framedata();
     let mut energy_total = 0.0;
-    for (_id, energy) in user_energies {
+    for (_id, energy) in simulation.user_energies() {
         if let Some(energy) = energy {
             energy_total += energy;
         }
@@ -179,8 +195,8 @@ pub fn run_simulation_thread(
     sim_clone: Arc<Mutex<FrameBroadcaster>>,
     state_clone: Arc<Mutex<StateBroadcaster>>,
     simulation_interval: u64,
-    frame_interval: u32,
-    force_interval: u32,
+    frame_interval: usize,
+    force_interval: usize,
     verbose: bool,
     mut playback_rx: Receiver<PlaybackOrder>,
     simulation_tx: std::sync::mpsc::Sender<usize>,
@@ -220,9 +236,6 @@ pub fn run_simulation_thread(
         }
         info!("Simulation interval: {simulation_interval}");
 
-        let mut user_energies: Vec<(Option<String>, Option<f64>)> = Vec::new();
-
-        let mut current_simulation_frame: u64 = 0;
         loop {
             let now = time::Instant::now();
             loop {
@@ -238,16 +251,9 @@ pub fn run_simulation_thread(
                     Ok(PlaybackOrder::Step) => {
                         if let Some(ref mut simulation) = maybe_simulation {
                             let delta_frames =
-                                next_frame_stop(current_simulation_frame, frame_interval);
+                                next_frame_stop(simulation.simulation_frame(), frame_interval);
                             simulation.step(delta_frames);
-                            current_simulation_frame += delta_frames as u64;
-                            if send_regular_frame(
-                                simulation,
-                                &user_energies,
-                                Arc::clone(&sim_clone),
-                            )
-                            .is_err()
-                            {
+                            if send_regular_frame(simulation, Arc::clone(&sim_clone)).is_err() {
                                 return;
                             }
                             playback_state.update(PlaybackOrder::Step);
@@ -305,26 +311,26 @@ pub fn run_simulation_thread(
 
             if let Some(ref mut simulation) = maybe_simulation {
                 if playback_state.is_playing() {
-                    let (delta_frames, do_frames, do_forces) =
-                        next_stop(current_simulation_frame, frame_interval, force_interval);
+                    let (delta_frames, do_frames, do_forces) = next_stop(
+                        simulation.simulation_frame(),
+                        frame_interval,
+                        force_interval,
+                    );
                     simulation.step(delta_frames);
-                    current_simulation_frame += delta_frames as u64;
 
                     if do_forces {
-                        let force_map;
-                        (force_map, user_energies) = apply_forces(
+                        let (force_map, user_energies) = apply_forces(
                             &state_clone,
                             simulation.simulation_mut(),
                             simulation_tx.clone(),
                         );
                         simulation.update_user_forces(force_map);
+                        simulation.update_user_energies(user_energies);
                     }
 
                     let system_energy = simulation.simulation.get_total_energy();
 
-                    if do_frames
-                        && send_regular_frame(simulation, &user_energies, Arc::clone(&sim_clone))
-                            .is_err()
+                    if do_frames && send_regular_frame(simulation, Arc::clone(&sim_clone)).is_err()
                     {
                         return;
                     };
@@ -336,7 +342,8 @@ pub fn run_simulation_thread(
                     };
                     if verbose {
                         info!(
-                            "Simulation frame {current_simulation_frame}. Time to sleep {time_left:?}. Total energy {system_energy:.2} kJ/mol."
+                            "Simulation frame {}. Time to sleep {time_left:?}. Total energy {system_energy:.2} kJ/mol.",
+                            simulation.simulation_frame(),
                         );
                     };
                     if auto_reset && !system_energy.is_finite() {
